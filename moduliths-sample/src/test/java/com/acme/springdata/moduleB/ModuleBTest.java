5318b70684c125a586c8066b5cedeeb975854fd0
Tom Hombergs <tom.hombergs@gmail.com>
2018-07-11 22:29:47 +0200

    added test for loading public repositories from another module (ignored because it's currently failing)

---
+++ moduliths-sample/src/test/java/com/acme/springdata/moduleB/ModuleBTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.springdata.moduleB;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.acme.myproject.NonVerifyingModuleTest;
+import com.acme.springdata.moduleA.RepositoryA;
+import com.acme.springdata.moduleA.internal.InternalRepositoryA;
+
+/**
+ * @author Tom Hombergs
+ */
+@NonVerifyingModuleTest
+@RunWith(SpringRunner.class)
+public class ModuleBTest {
+
+	@Autowired ApplicationContext context;
+
+	@Test
+	@Ignore
+	public void bootstrapsModuleBAndDependentsFromModuleAOnly() {
+
+		context.getBean(RepositoryB.class);
+		context.getBean(RepositoryA.class);
+
+		assertThatExceptionOfType(NoSuchBeanDefinitionException.class)
+				.isThrownBy(() -> context.getBean(InternalRepositoryA.class));
+	}
+}
