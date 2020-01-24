7c3ea7b798ffd818e849c483f1ac86f792a6acdb
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-01-24 16:11:54 +0100

    #98 - Improve event capture during test cases.
    We now avoid registering an additional event listener for each test method that takes a PublishedEvents parameter. We register one initially and then for each method invocation we register a PublishedEvents instance to and bind it to the current thread. We unregister that instance after the test methods has been concluded. Using a thread bound delegate makes sure that this also works if tests are executed concurrently.
    
    Original pull request: #99.

--- moduliths-test/src/main/java/org/moduliths/test/PublishedEventsParameterResolver.java
+++ moduliths-test/src/main/java/org/moduliths/test/PublishedEventsParameterResolver.java
@@ -15,28 +15,55 @@
  */
 package org.moduliths.test;
 
+import java.util.function.Function;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
 
 /**
  * Provides instances of {@link PublishedEvents} as test method parameters.
  *
  * @author Oliver Drotbohm
  */
-class PublishedEventsParameterResolver implements ParameterResolver {
+class PublishedEventsParameterResolver implements ParameterResolver, BeforeAllCallback, AfterEachCallback {
+
+	private ThreadBoundApplicationListenerAdapter listener = new ThreadBoundApplicationListenerAdapter();
+	private final Function<ExtensionContext, ApplicationContext> lookup;
+
+	PublishedEventsParameterResolver() {
+		this(ctx -> SpringExtension.getApplicationContext(ctx));
+	}
+
+	PublishedEventsParameterResolver(Function<ExtensionContext, ApplicationContext> supplier) {
+		this.lookup = supplier;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.junit.jupiter.api.extension.BeforeAllCallback#beforeAll(org.junit.jupiter.api.extension.ExtensionContext)
+	 */
+	@Override
+	public void beforeAll(ExtensionContext extensionContext) {
+
+		ApplicationContext context = lookup.apply(extensionContext);
+		((ConfigurableApplicationContext) context).addApplicationListener(listener);
+	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.junit.jupiter.api.extension.ParameterResolver#supportsParameter(org.junit.jupiter.api.extension.ParameterContext, org.junit.jupiter.api.extension.ExtensionContext)
 	 */
 	@Override
-	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
-			throws ParameterResolutionException {
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
 		return PublishedEvents.class.isAssignableFrom(parameterContext.getParameter().getType());
 	}
 
@@ -45,14 +72,66 @@ class PublishedEventsParameterResolver implements ParameterResolver {
 	 * @see org.junit.jupiter.api.extension.ParameterResolver#resolveParameter(org.junit.jupiter.api.extension.ParameterContext, org.junit.jupiter.api.extension.ExtensionContext)
 	 */
 	@Override
-	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
-			throws ParameterResolutionException {
+	public PublishedEvents resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
 
 		DefaultPublishedEvents publishedEvents = new DefaultPublishedEvents();
-
-		ApplicationContext context = SpringExtension.getApplicationContext(extensionContext);
-		((ConfigurableApplicationContext) context).addApplicationListener(publishedEvents);
+		listener.registerDelegate(publishedEvents);
 
 		return publishedEvents;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.junit.jupiter.api.extension.AfterEachCallback#afterEach(org.junit.jupiter.api.extension.ExtensionContext)
+	 */
+	@Override
+	public void afterEach(ExtensionContext context) {
+		listener.unregisterDelegate();
+	}
+
+	/**
+	 * {@link ApplicationListener} that allows registering delegate {@link ApplicationListener}s that are held in a
+	 * {@link ThreadLocal} and get used on {@link #onApplicationEvent(ApplicationEvent)} if one is registered for the
+	 * current thread. This allows multiple event listeners to see the events fired in a certain thread in a concurrent
+	 * execution scenario.
+	 *
+	 * @author Oliver Drotbohm
+	 */
+	private static class ThreadBoundApplicationListenerAdapter implements ApplicationListener<ApplicationEvent> {
+
+		private final ThreadLocal<ApplicationListener<ApplicationEvent>> delegate = new ThreadLocal<>();
+
+		/**
+		 * Registers the given {@link ApplicationListener} to be used for the current thread.
+		 *
+		 * @param listener must not be {@literal null}.
+		 */
+		void registerDelegate(ApplicationListener<ApplicationEvent> listener) {
+
+			Assert.notNull(listener, "Delegate ApplicationListener must not be null!");
+
+			delegate.set(listener);
+		}
+
+		/**
+		 * Removes the registration of the currently assigned {@link ApplicationListener}.
+		 */
+		void unregisterDelegate() {
+			delegate.remove();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
+		 */
+		@Override
+		public void onApplicationEvent(ApplicationEvent event) {
+
+			ApplicationListener<ApplicationEvent> listener = delegate.get();
+
+			if (listener != null) {
+				listener.onApplicationEvent(event);
+			}
+		}
 	}
 }
