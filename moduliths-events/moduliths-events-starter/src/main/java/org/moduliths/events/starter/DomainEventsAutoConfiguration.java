e461a2ac9e824aa1a42b24dda51b1fdf53dece88
Oliver Drotbohm <odrotbohm@vmware.com>
2020-11-25 15:59:14 +0100

    #121 - Fix package declaration of Spring Boot starter configuration.

--- moduliths-events/moduliths-events-starter/src/main/java/org/moduliths/events/starter/DomainEventsAutoConfiguration.java
+++ moduliths-events/moduliths-events-starter/src/main/java/org/moduliths/events/starter/DomainEventsAutoConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.events.starter;
+package org.moduliths.events.starter;
 
 import org.moduliths.events.EventPublication;
 import org.moduliths.events.config.EnablePersistentDomainEvents;
--- moduliths-events/moduliths-events-starter/src/main/java/org/springframework/events/starter/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.springframework.lang.NonNullApi
-package org.springframework.events.starter;
